1658618281f1907c336e030981e35aed157b5994
ryo <ryoga@kawa-e.com>
2024-12-04 17:13:26 +0900

    テキスト表示に関する修正(ボタンにノードをアタッチ,要素取得のパス指定修正)

--- TestObject_Button.gd
+++ TestObject_Button.gd
@@ -1,18 +1,19 @@
 extends Button
 
 # ボタンが押されたときの処理
-func _on_button_pressed():
+func _on_button_down():
 	# Camera2D ノードを取得
-	var camera = get_parent().get_node("res://Camera2D")
+	var camera = get_node("/root/Main/Camera2D")
 	if not camera:
 		print("Camera2D ノードが見つかりません")
 		return
 	
 	# Label ノードを取得
-	var label = camera.get_node("res://Control/Panel/Label")
+	var label = get_node("/root/Main/Camera2D/Control/Panel/Label")
 	if not label:
 		print("Label ノードが見つかりません")
 		return
 	
 	# Label ノードの show_message メソッドを呼び出し
-	label.show_message("text")  # ボタンに設定されたテキストを渡す
+	label.show_message(get_node("/root/Main/SampleHaikei/Button").text)  # ボタンに設定されたテキストを渡す
+	
--- label.gd
+++ label.gd
@@ -4,7 +4,9 @@ var is_visible = false  # テキスト欄が表示されているかどうか
 var display_duration = 3.0  # テキスト表示の持続時間（秒）
 
 func show_message(text: String):
+	#引数に入力されたテキストを,変数textに格納
 	self.text = text
+	
 	is_visible = true
 	visible = true
 	# 一定時間後に非表示にする
